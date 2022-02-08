efa311d39d302a029fcfd72353aa270f3e0b1947
wjdgus9808 <38302470+wjdgus9808@users.noreply.github.com>
2022-02-08 16:56:40 +0900

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+// Switch->Routes로 변경
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
